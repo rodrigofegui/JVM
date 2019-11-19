4316519bd0e65ad04f913edb2f627ca608d75206
rodrigofegui <rodrigofegui@gmail.com>
2019-11-19 20:54:25 -0300

    Edit: Gerar sementes sem armazená-las; ASM para upperCase

--- asm/lsfr.asm
+++ asm/lsfr.asm
@@ -4,6 +4,13 @@
 ;-------------------------------------------------------------------------------
 %include "asm_io.inc"
 ;-------------------------------------------------------------------------------
+; Definições, cópias do prog em C
+;-------------------------------------------------------------------------------
+%define MAX_PERIODO 16777216
+%define QNT_GRUPOS  16
+%define FREQ_ESP    (MAX_PERIODO / QNT_GRUPOS)
+%define N_BITS      23
+;-------------------------------------------------------------------------------
 ; Variáveis definidas
 ;-------------------------------------------------------------------------------
 segment .data
@@ -11,6 +18,10 @@ segment .data
 ; Variáveis reservadas
 ;-------------------------------------------------------------------------------
 segment .bss
+    lsfr        resd    1
+    resultado   resd    1
+    periodo     resd    1
+    bit         resw    1
 ;-------------------------------------------------------------------------------
 ; Programa
 ;-------------------------------------------------------------------------------
@@ -22,24 +33,56 @@ segment .text
         global  _calc_lsfr_asm
     _calc_lsfr_asm:
     %endif
-        enter   0,0
-        pusha
+        ENTER   0,0
+        PUSHA
 
-        push    EBP
-        mov     EBP, ESP
+        PUSH    EBP
+        MOV     EBP, ESP
 
         ; CÓDIGO AQUI
-        mov     EBX, [EBP]          ; Endereço de endereço
-        mov     EBX, [EBX + 8]      ; A freq. está vindo como último parâmetro
-        mov     EBX, [EBP]
-        mov     EAX, [EBX + 12]     ; A semente
+        ; MOV     EBX, [EBP]          ; Endereço de endereço
+        ; MOV     EBX, [EBX + 8]      ; A freq. está vindo como último parâmetro
+        MOV     EBX, [EBP]
+        MOV     EBX, [EBX + 12]     ; A semente
+        MOV     [lsfr], EBX
+        MOV     EDX, 0
+        MOV     EDX, [lsfr]
+        RCR     EDX, 1
+        SALC
+        AND     AL, 0x01
+        MOV     CL, AL
+        dump_regs 1
 
-        pop     EBP
+        MOV     EDX, [lsfr]
+        RCR     EDX, 17
+        SALC
+        AND     AL, 0x01
+        XOR     AL, CL
+        MOV     CL, AL
+    ;     MOV     [bit], word 0
+    ;     MOV     EDX, 0
+    ; .lsfr_loop:
+    ;     MOV     CL, 20
+    ;     MOV     EAX, [lsfr]
+    ;     RCR     EAX, CL
+    ;     SALC
+    ;     XOR     AL, 0x01
+    ;     ADD     AL, [bit]
+    ;     MOV     [bit], word AL
+    ;     MOV     DL, [bit]
+        dump_regs 2
 
-        popa
+        ; MOV     EAX, 0
+        ; SALC
+        ; CALL    print_int
+        ; CALL    print_nl
+
+        POP     EBP
+
+        POPA
         ; mov     eax, 0            ; Só se tiver retorno
-        leave
-        ret
+        LEAVE
+        RET
 
 ; 32   28   24   20   16   12   8    4
 ; 0000 0000 1110 0001 0000 0000 0000 0001
